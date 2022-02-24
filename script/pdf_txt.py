8e0119c40091a9dff98ed885ab28ad21d599f94e
cesare-spinoso <cesare.spinoso@gmail.com>
2022-02-23 21:06:35 -0500

    updated extraction script

--- script/pdf_txt.py
+++ script/pdf_txt.py
@@ -2,7 +2,10 @@ import argparse
 import os
 from pathlib import Path
 from typing import Dict, Tuple
+from PIL import Image
 
+import pdf2image
+import pytesseract
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LAParams, LTTextBox, LTTextLine
 from pdfminer.pdfdocument import PDFDocument
@@ -10,10 +13,18 @@ from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
+REPORT_TYPES = {"surgical", "ultrasound", "pathology"}
+
 
 def read_args() -> Tuple[Path, Path]:
     parser = argparse.ArgumentParser(
         description="Convert pdf files in a directory to text files with the same name."
+    )
+    parser.add_argument(
+        "-r",
+        "--report_type",
+        help=f"The type of report to be converted. Accepts one of {REPORT_TYPES}",
+        required=True,
     )
     parser.add_argument(
         "-p",
@@ -24,22 +35,54 @@ def read_args() -> Tuple[Path, Path]:
     parser.add_argument(
         "-t",
         "--text_folder_output_path",
-        help="The folder path that will be sued to write out the converted text files.",
+        help="The folder path that will be used to write out the converted text files.",
         required=True,
     )
+    parser.add_argument(
+        "-i",
+        "--image_folder_output_path",
+        help="The folder path that will be used to save the images of the pdf"
+        + " (not required for ultrasound reports).",
+        required=False,
+    )
     args = parser.parse_args()
+    report_type = args.report_type
     pdf_folder_input_path = Path(args.pdf_folder_input_path)
     text_folder_output_path = Path(args.text_folder_output_path)
+    image_folder_output_path = args.image_folder_output_path
+    assert report_type in REPORT_TYPES, f"Invalid report type: {report_type}"
     assert (
         pdf_folder_input_path.exists() and pdf_folder_input_path.is_dir()
     ), "The input pdf folder does not exist or is not a folder directory."
     if not text_folder_output_path.exists():
-        print("The output text folder does not exists, creating it.")
+        print("The output text folder does not exist, creating it.")
         os.makedirs(text_folder_output_path)
-    return pdf_folder_input_path, text_folder_output_path
+    if report_type != "ultrasound":
+        assert (
+            image_folder_output_path is not None
+        ), "The image folder output path is required for {report_type} report types."
+        image_folder_output_path = Path(image_folder_output_path)
+        if not image_folder_output_path.exists():
+            print("The output image folder does not exist, creating it.")
+            os.makedirs(image_folder_output_path)
+    return (
+        report_type,
+        pdf_folder_input_path,
+        text_folder_output_path,
+        image_folder_output_path,
+    )
 
 
-def read_pdf_text(pdf_file_path: Path) -> Dict[str, str]:
+def read_pdf_text(
+    pdf_file_path: Path, report_type: str, image_output_folder_path: Path
+) -> Dict[str, str]:
+    if report_type == "ultrasound":
+        return read_pdf_text_directly(pdf_file_path)
+    else:
+        return read_pdf_text_indirectly(pdf_file_path, image_output_folder_path)
+
+
+def read_pdf_text_directly(pdf_file_path: Path) -> Dict[str, str]:
     pdf_text_dict = {}
     for path in Path(pdf_file_path).glob("*.pdf"):
         with path.open("rb") as file:
@@ -65,6 +108,26 @@ def read_pdf_text(pdf_file_path: Path) -> Dict[str, str]:
     return pdf_text_dict
 
 
+def read_pdf_text_indirectly(
+    pdf_file_path: Path, image_output_folder_path: Path
+) -> Dict[str, str]:
+    pdf_text_dict = {}
+    for path in Path(pdf_file_path).glob("*.pdf"):
+        file_path_stem = path.stem
+        pdf_text_dict[file_path_stem] = []
+        # Convert the pdf to images
+        images = pdf2image.convert_from_path(path)
+        for i, image in enumerate(images):
+            image_path = os.path.join(
+                image_output_folder_path, f"{file_path_stem}_{i}.png"
+            )
+            image.save(image_path, "PNG")
+            img = Image.open(image_path)
+            pdf_text_dict[file_path_stem].append(pytesseract.image_to_string(img))
+        pdf_text_dict[file_path_stem] = "".join(pdf_text_dict[file_path_stem])
+    return pdf_text_dict
+
+
 def write_text(pdf_text_dict: Dict[str, str], text_folder_output_path: Path) -> None:
     for path_stem, text in pdf_text_dict.items():
         output_file_path = os.path.join(text_folder_output_path, f"{path_stem}.txt")
@@ -74,27 +137,34 @@ def write_text(pdf_text_dict: Dict[str, str], text_folder_output_path: Path) -> 
 
 def main():
     """
-    Goal of this python script is to extract the text from pdf reports 
+    Goal of this python script is to extract the text from pdf reports
     and create a txt version of it with the same name.
 
     Example:
-    python pdf_txt.py --pdf_folder_input_path=pdf --text_folder_output_path=txt
+    python pdf_txt.py --report_type=ultrasound --pdf_folder_input_path=pdf --text_folder_output_path=txt
     Input:
-    pdf
-     ├─a.pdf
-     ├─b.pdf
-     └─c.pdf
+        pdf
+        ├─a.pdf
+        ├─b.pdf
+        └─c.pdf
     Result:
-    txt
-     ├─a.txt
-     ├─b.txt
-     └─c.txt
+        txt
+        ├─a.txt
+        ├─b.txt
+        └─c.txt
     """
     # Read command line arguments
-    pdf_folder_input_path, text_folder_output_path = read_args()
+    (
+        report_type,
+        pdf_folder_input_path,
+        text_folder_output_path,
+        image_output_folder_path,
+    ) = read_args()
     # Read pdf files and convert to text files
     print("Reading pdf files...")
-    pdf_text_dict = read_pdf_text(pdf_folder_input_path)
+    pdf_text_dict = read_pdf_text(
+        pdf_folder_input_path, report_type, image_output_folder_path
+    )
     # Write out each text file using the same pdf file name
     print("Writing text files...")
     write_text(pdf_text_dict, text_folder_output_path)
